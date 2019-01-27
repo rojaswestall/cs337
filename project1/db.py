e5bf1239aa30c80742f379946e3fc1f60c678239
Gabe Rojas-Westall <rojaswestall@gmail.com>
2019-01-27 15:03:43 -0600

    getting some data

--- project1/db.py
+++ project1/db.py
@@ -1,7 +1,22 @@
 from pymongo import MongoClient
+import pymongo
 
 # Run bash script for mongo setup ...
 
 client = MongoClient()
 
-db = client['test-database']
+db = client['gg-twitter']
+
+# tweets2015 = db['tweets-2015'].find_one({"text": 554403687448469504})
+
+db['tweets-2015'].create_index([("text", pymongo.TEXT)])
+
+tweets2015 = db['tweets-2015'].find({ "$text": { "$search": "host golden globes"}})
+
+for i, doc in enumerate(tweets2015):
+	if i > 10: break
+	print(doc)
+
+print(tweets2015)
+
+# tweets-2015 is collection
--- project1/setup.sh
+++ project1/setup.sh
@@ -1,2 +1,2 @@
-mongoimport --db test-database --collection tweets-2015 --file gg2015.json --jsonArray
-mongoimport --db test-database --collection tweets-2013 --file gg2013.json --jsonArray
+mongoimport --db gg-twitter --collection tweets-2015 --file gg2015.json --jsonArray
+mongoimport --db gg-twitter --collection tweets-2013 --file gg2013.json --jsonArray
