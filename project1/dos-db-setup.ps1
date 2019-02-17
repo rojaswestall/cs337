076bcd0410fe7df6d95dc28a19a73b8c295a604c
Jeff Holm <jholm117@gmail.com>
2019-02-16 20:58:25 -0600

    imports json data to mongo in pre_ceremony

--- project1/dos-db-setup.ps1
+++
@@ -1,7 +0,0 @@
-$config = Get-Content -Raw -Path config.json | ConvertFrom-Json
-$jsonPath = $config."pathToTweets"
-$dbName = $config."dbName"
-$dbCollection = $config."dbCollection"
-# $jsonFileName = [io.path]::GetFileNameWithoutExtension($jsonPath)
-mongoimport --db $dbName --collection $dbCollection --file $jsonPath --jsonArray
-mongo db-setup.js
