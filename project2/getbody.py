825dac9e11f05edb06c96a88905c06013020cc5c
David Latimore II <david.latimore@me.com>
2019-02-23 18:32:36 -0600

    cleaning code

--- project2/getbody.py
+++ project2/getbody.py
@@ -5,11 +5,8 @@ from bs4 import BeautifulSoup
 response = urllib2.urlopen('https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/?internalSource=hub%20recipe&referringContentType=Search')
 soup = BeautifulSoup(response, 'html.parser')
 
-ingredients1 = soup.find(id="lst_ingredients_1").text
-ingredients2 = soup.find(id="lst_ingredients_2").text
-
-print(ingredients1)
-print(ingredients2)
+print(soup.find(id="lst_ingredients_1").text)
+print(soup.find(id="lst_ingredients_2").text)
 
 for step in soup.select('span[class*="recipe-directions__list--item"]'):
     print step.text
