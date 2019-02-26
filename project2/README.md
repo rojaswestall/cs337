2bad0ba7b58a38a93057e2a1965fe9867ddc2e60
Jeff Holm <jholm117@gmail.com>
2019-02-26 13:21:42 -0600

    adds README with installation instructions and creates creates pipenv environment

---
+++ project2/README.md
@@ -0,0 +1,43 @@
+# Recipe Transposer
+
+## Installation
+
+This project uses [`pipenv`](https://pipenv.readthedocs.io/en/latest/) to manage dependencies. Make sure you have it installed before working on the project:
+
+```sh
+pip install --user pipenv
+```
+
+First navigate to the `project2` directory. Then install dependencies:
+
+```sh
+pipenv install
+```
+
+Now activate the virtual environment:
+
+```sh
+pipenv shell
+```
+
+Now you should be good to go.
+
+## Using `pipenv`
+
+Adding dependencies:
+
+```sh
+pipenv install <package-name>
+```
+
+Removing dependencies:
+
+```sh
+pipenv uninstall <package-name>
+```
+
+Exiting virtual environment:
+
+```sh
+exit
+```
--- project2/getbody.py
+++ project2/getbody.py
@@ -1,12 +1,12 @@
 import html2text
-import urllib2
+import urllib.request as url
 from bs4 import BeautifulSoup
 
-response = urllib2.urlopen('https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/?internalSource=hub%20recipe&referringContentType=Search')
+response = url.urlopen('https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/?internalSource=hub%20recipe&referringContentType=Search')
 soup = BeautifulSoup(response, 'html.parser')
 
 print(soup.find(id="lst_ingredients_1").text)
 print(soup.find(id="lst_ingredients_2").text)
 
 for step in soup.select('span[class*="recipe-directions__list--item"]'):
-    print step.text
+    print(step.text)
